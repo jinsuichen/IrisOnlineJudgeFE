2d46b7e074468a5887fb353b8ebf85c9d1a83ff7
jinsuichen <ischenjinsui@gmail.com>
2023-05-06 00:42:58 +0800

    feat: Use items instead of children on Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,30 @@
 import {Component} from 'react';
 import {Menu} from 'antd';
+import './index.scss'
+import {NavLink} from "react-router-dom";
 
 class Navbar extends Component {
     render() {
+
+        const menuItems = [
+            {
+                key: 'home',
+                label: (<NavLink to={'/'}>{'Home'}</NavLink>),
+            },
+            {
+                key: 'problems',
+                label: (<NavLink to={'/problems'}>{'Problems'}</NavLink>),
+            },
+            {
+                key: 'profile',
+                label: (<NavLink to={'/profile'}>{'Profile'}</NavLink>),
+            },
+        ];
+
+
         return (
             <div>
-                <Menu mode="horizontal" activeKey={'home'} style={{marginBottom: "20px"}}>
-                    <Menu.Item key="home" style={{marginLeft: "20%"}}>
-                        <a href="/">Home</a>
-                    </Menu.Item>
-                    <Menu.Item key="problems">
-                        <a href="/problems">Problems</a>
-                    </Menu.Item>
-                    <Menu.Item key="profile" style={{marginLeft: 'auto', marginRight: "20%"}}>
-                        <a href="/profile">Profile</a>
-                    </Menu.Item>
-                </Menu>
+                <Menu mode="horizontal" activeKey={'home'} style={{marginBottom: "20px"}} items={menuItems}/>
             </div>
         );
     }
